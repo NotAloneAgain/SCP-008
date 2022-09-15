68f042b38a549beaee541fdc16ce51b148eaef21
AlexanderK <73738345+AlexanderK666@users.noreply.github.com>
2022-09-15 16:44:17 +0300

    Init project

---
+++ Scp008/Config.cs
@@ -0,0 +1,15 @@
+﻿namespace Scp008
+{
+    using System.ComponentModel;
+    using Exiled.API.Interfaces;
+
+    /// <summary>
+    /// Plugin config class.
+    /// </summary>
+    public class Config : IConfig
+    {
+        /// <inheritdoc/>
+        [Description("")]
+        public bool IsEnabled { get; set; } = true;
+    }
+}
---
+++ Scp008/Scp008.cs
@@ -0,0 +1,28 @@
+﻿namespace Scp008
+{
+    using System;
+    using Exiled.API.Features;
+
+    /// <inheritdoc/>
+    public class Scp008 : Plugin<Config>
+    {
+        /// <inheritdoc/>
+        public override Version RequiredExiledVersion => new Version(5, 3, 0);
+
+        /// <inheritdoc/>
+        public override void OnEnabled()
+        {
+
+
+            base.OnEnabled();
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisabled()
+        {
+
+
+            base.OnDisabled();
+        }
+    }
+}
